17764d34ecf425a9e0350e37c4e5c6e1929af55b
David Morales <David_Morales@epam.com>
2017-01-19 14:18:16 -0600

    this.props.children in React and React Router

---
+++ app/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = (props) => {
+  return (
+    <div>
+      Hello from Home!
+    </div>
+
+  )
+}
+
+export default Home
--- app/index.js
+++ app/index.js
@@ -1,45 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const USER_DATA = {
-  name: 'David',
-  username: 'dmoralesm',
-  image: 'https://avatars3.githubusercontent.com/u/5688757?v=3&s=460'
-}
-
-const ProfilePic = (props) => {
-  return (
-    <img src={props.imageUrl} style={{height: 100, width: 100}} />
-  )
-}
-
-const ProfileLink = (props) => {
-  return (
-    <div>
-      <a href={`https://www.github.com/${props.username}`}>
-        {props.username}
-      </a>
-    </div>
-  )
-}
-
-const ProfileName = (props) => {
-  return (
-    <div>{props.name}</div>
-  )
-}
-
-const Avatar = (props) => {
-  return (
-    <div>
-      <ProfilePic imageUrl={props.user.image}/>
-      <ProfileName name={props.user.name}/>
-      <ProfileLink username={props.user.username}/>
-    </div>
-  )
-}
+import routes from './config/routes'
 
 ReactDOM.render(
-  <Avatar user={USER_DATA}/>,
+  routes,
   document.getElementById('app')
 )
