0dce1790294a17dcecf2d5fe15e86b731bf94d90
David Morales <David_Morales@epam.com>
2017-01-19 12:56:43 -0600

    Pure Functions, props, and nesting React components in React

--- app/index.js
+++ app/index.js
@@ -1,13 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const HelloWorld = (props) => {
+const USER_DATA = {
+  name: 'David',
+  username: 'dmoralesm',
+  image: 'https://avatars3.githubusercontent.com/u/5688757?v=3&s=460'
+}
+
+const ProfilePic = (props) => {
   return (
-    <h1>Hello world.</h1>
+    <img src={props.imageUrl} style={{height: 100, width: 100}} />
+  )
+}
+
+const ProfileLink = (props) => {
+  return (
+    <div>
+      <a href={`https://www.github.com/${props.username}`}>
+        {props.username}
+      </a>
+    </div>
+  )
+}
+
+const ProfileName = (props) => {
+  return (
+    <div>{props.name}</div>
+  )
+}
+
+const Avatar = (props) => {
+  return (
+    <div>
+      <ProfilePic imageUrl={props.user.image}/>
+      <ProfileName name={props.user.name}/>
+      <ProfileLink username={props.user.username}/>
+    </div>
   )
 }
 
 ReactDOM.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA}/>,
   document.getElementById('app')
 )
